ccd5d9cdfefc306ad60ae1f881b84a94b10526af
LucasPagli <lucas.dsilva@sptech.school>
2024-10-21 10:02:29 -0300

    refactor: Ajustes gerais e Responsividade AgendarConsulta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Introduction from '../Sections/Introduction/Introduction';
 import ProcedureList from '../Sections/ProcedureList/ProcedureList';
 import DentalProcedures from '../Sections/DentalProcedures/DentalProcedures';
 import TeamSection from '../Sections/TeamSection/TeamSection';
+import AgendarConsulta from '../Sections/AgendarConsulta/AgendarConsulta';
 import style from './Home.module.css';
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
       <ProcedureList />
       <DentalProcedures />
       <TeamSection />
+      <AgendarConsulta />
     </div>
   );
 };
